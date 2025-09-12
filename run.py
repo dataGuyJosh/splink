d8a6bed0fead1d0df92caae18426c9756fe93302
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2025-09-12 19:54:41 +1000

    figured out altair chart rendering issues

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from splink import  splink_datasets
-from splink.exploratory import completeness_chart
+from splink.exploratory import completeness_chart, profile_columns
 from splink import DuckDBAPI
+import altair as alt
+
+
+alt.renderers.enable('browser')
 
 df = splink_datasets.fake_1000
 df = df.drop(columns=["cluster"])
 # print(df.head(5))
 
 db_api = DuckDBAPI()
+
 completeness_chart(df, db_api=db_api).show()
+profile_columns(df, db_api=DuckDBAPI(), top_n=10, bottom_n=5).show()
